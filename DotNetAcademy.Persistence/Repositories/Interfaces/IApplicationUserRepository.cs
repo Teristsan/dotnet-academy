33c98bdca6e96d0f60ef7f0397c41a19f6112406
Lefteris Tsantikos (GR) <lefteris.tsantikos@pwc.com>
2025-11-21 16:19:07 +0200

    Configured login along with the domain

--- DotNetAcademy.Persistence/Repositories/Interfaces/IApplicationUserRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using DotNetAcademy.Persistence.Entities;
-
-namespace DotNetAcademy.Persistence.Repositories.Interfaces;
-
-public interface IApplicationUserRepository
-{
-    Task<ApplicationUser?> GetUserByIdAsync(string id);
-    Task UpdateUserFieldsAsync(ApplicationUser user);
-}
--- DotNetAcademy/Program.cs
+++ DotNetAcademy/Program.cs
@@ -1,11 +1,6 @@
 using DotNetAcademy.Components;
 using DotNetAcademy.Persistence;
 using DotNetAcademy.Persistence.Entities;
-using DotNetAcademy.Persistence.Repositories;
-using DotNetAcademy.Persistence.Repositories.Interfaces;
-using DotNetAcademy.Services.ApplicationUserService;
-using DotNetAcademy.Services.ItemService;
-using DotNetAcademy.Services.ItemsService;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -38,15 +33,6 @@ builder.Services.ConfigureApplicationCookie(options =>
 	options.AccessDeniedPath = "/Account/AccessDenied";
 	options.SlidingExpiration = true;
 });
-
-
-//Repositories
-builder.Services.AddScoped<IItemRepository, ItemRepository>();
-builder.Services.AddScoped<IApplicationUserRepository, ApplicationUserRepository>();
-
-//Services
-builder.Services.AddScoped<IItemService, ItemService>();
-builder.Services.AddScoped<IApplicationUserService, ApplicationUserService>();
 
 var app = builder.Build();
 
